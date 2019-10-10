b7f3fba3c7dd30ec0ab9e767a1122b26d0938382
bilal <bilal.soft000@gmail.com>
2019-10-10 16:02:31 +0500

    hover backgorund color

--- docs/js/dark-mode.js
+++ docs/js/dark-mode.js
@@ -1,10 +1,8 @@
 var nightModeToggleButton = document.querySelector(".nightmode");
 var h1 = document.querySelector("h1");
 var h3 = document.querySelector("h3");
-var span = document.querySelector("span");
 var row = document.querySelector(".row-parent");
 var body = document.querySelector("body");
-var img = document.querySelector(".row-parent .column img");
 
 nightModeToggleButton.onclick = function() {
   nightModeToggleButton.classList.toggle("night-mode");
@@ -13,14 +11,3 @@ nightModeToggleButton.onclick = function() {
   h1.classList.toggle("night-mode");
   img.classList.toggle("night-mode");
 };
-
-[].map.call(document.querySelectorAll(".row-parent .column img"), function(el) {
-  el.onmouseenter = function() {
-    let title = el.getAttribute("title");
-    span.textContent = "@" + title;
-  };
-
-  el.onmouseleave = function() {
-    span.textContent = "";
-  };
-});
