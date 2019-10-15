594d62aad32bc0470a3f45be9bdd06dd673e5857
maydayo <wanwisa.silanan@gmail.com>
2019-10-16 02:51:22 +0700

    remove console.log

--- docs/js/vibrate.js
+++ docs/js/vibrate.js
@@ -4,7 +4,6 @@ function makeItVibrate(event) {
   squares.forEach(element => {
     element.classList.add("vibrate");
     let duration = Math.random() * 0.2+0.3;
-    console.log(duration)
     let direction = ["shakeX", "shakeY"][Math.floor(Math.random() * 2)];
     element.style.setProperty("--duration", `${duration}s`);
     element.style.setProperty("--direction", `${direction}`);
